6d153a2ebbb6f3e88fe45557191404af4f3877d7
Farid <fred.andersen7@gmail.com>
2020-05-23 21:58:49 +0600

    Created bottom tabs navigation

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 // React Navigation
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 // Screens
 import CategoryScreen from '../screens/CategoryScreen'
@@ -12,18 +13,58 @@ import FoodScreen from '../screens/FoodScreen'
 import ProfileScreen from '../screens/ProfileScreen'
 
 const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
-export class AppNavigation extends Component {
-	render() {
-		return (
-			<NavigationContainer>
-				<Stack.Navigator>
-					<Stack.Screen name='Category' component={CategoryScreen} />
-					<Stack.Screen name='FAQ' component={FAQScreen} />
-					<Stack.Screen name='Food' component={FoodScreen} />
-					<Stack.Screen name='Profile' component={ProfileScreen} />
-				</Stack.Navigator>
-			</NavigationContainer>
-		)
-	}
+// Stacks Navigation
+const CategoryNavigation = () => {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name='Category' component={CategoryScreen} />
+		</Stack.Navigator>
+	)
+}
+
+const FAQNavigation = () => {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name='FAQ' component={FAQScreen} />
+		</Stack.Navigator>
+	)
+}
+
+const FoodNavigation = () => {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name='Food' component={FoodScreen} />
+		</Stack.Navigator>
+	)
+}
+
+const ProfileNavigation = () => {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name='Profile' component={ProfileScreen} />
+		</Stack.Navigator>
+	)
+}
+
+// Tabs Navigation
+const MainBottomTabsNavigation = () => {
+	return (
+		<Tab.Navigator>
+			<Tab.Screen name='Category' component={CategoryNavigation} />
+			<Tab.Screen name='FAQ' component={FAQNavigation} />
+			<Tab.Screen name='Food' component={FoodNavigation} />
+			<Tab.Screen name='Profile' component={ProfileNavigation} />
+		</Tab.Navigator>
+	)
+}
+
+// Navigation Container
+export const AppNavigation = () => {
+	return (
+		<NavigationContainer>
+			<MainBottomTabsNavigation />
+		</NavigationContainer>
+	)
 }
