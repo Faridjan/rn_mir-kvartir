421525a068cd292b10c9d66f31c53c6a05657a57
Farid <fred.andersen7@gmail.com>
2020-06-25 16:35:26 +0600

    Merged conflict fixed

--- src/screens/catalog/BookingScreen.js
+++ src/screens/catalog/BookingScreen.js
@@ -1,6 +1,16 @@
 // React
 import React, { Component } from 'react'
-import { Text, StyleSheet, ScrollView, View, KeyboardAvoidingView, Picker, TouchableOpacity, ActivityIndicator, Platform } from 'react-native'
+import {
+	Text,
+	StyleSheet,
+	ScrollView,
+	View,
+	KeyboardAvoidingView,
+	Picker,
+	TouchableOpacity,
+	ActivityIndicator,
+	Platform,
+} from 'react-native'
 
 // Components
 import Container from 'src/components/Container'
@@ -95,125 +105,39 @@ export default class BookingScreen extends Component {
 			<Container style={styles.container}>
 				{/* <Text style={styles.title}> Бронирование</Text> */}
 				<ScrollView>
-					<KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={Platform.select({ ios: 200, android: 0 })}>
+					<KeyboardAvoidingView
+						behavior='padding'
+						keyboardVerticalOffset={Platform.select({ ios: 200, android: 0 })}
+					>
 						<Text style={styles.title}>Квартира:</Text>
 						<View style={{ ...styles.picker }}>
-							<Picker selectedValue={type} onValueChange={(itemValue, itemIndex) => this.setState({ type: itemValue })}>
-								<Picker.Item label="Час" value={variations.hour.id} />
-								<Picker.Item label="Ночь" value={variations.night.id} />
-								<Picker.Item label="Сутки" value={variations.day.id} />
+							<Picker
+								selectedValue={type}
+								onValueChange={(itemValue, itemIndex) => this.setState({ type: itemValue })}
+							>
+								<Picker.Item label='Час' value={variations.hour.id} />
+								<Picker.Item label='Ночь' value={variations.night.id} />
+								<Picker.Item label='Сутки' value={variations.day.id} />
 							</Picker>
 						</View>
-<<<<<<< HEAD
-						<View style={styles.halfInput}>
-							<View style={{ flex: 1, marginRight: 10 }}>
-								{/* <Input
-									label='Дата заезда'
-									value={arrival}
-									onChangeText={(value) => this.setState({ arrival: value })}
-								/> */}
-								<TouchableOpacity
-									style={{ ...styles.picker, ...styles.input }}
-									onPress={() => this.setState({ showDatePickerArival: true })}
-								>
-									<Fontisto name='date' size={16} color='black' style={{ marginRight: 10 }} />
-									<Text>{arrival.format('DD-MM-YYYY')}</Text>
-									<Text style={styles.labelDate}>Дата заезда</Text>
-								</TouchableOpacity>
-								{showDatePickerArival && (
-									<DateTimePicker
-										testID='dateTimePicker'
-										value={moment(arrival).toDate()}
-										mode='date'
-										is24Hour={true}
-										minimumDate={moment(arrival).toDate()}
-										display='default'
-										onChange={(event, selectedDate) => {
-											this.setState({
-												arrival: moment(selectedDate),
-												showDatePickerArival: false,
-											})
-										}}
-									/>
-								)}
-							</View>
-							<View style={{ width: '50%' }}>
-								<TouchableOpacity
-									style={{ ...styles.picker, ...styles.input }}
-									onPress={() => this.setState({ showDatePickerDeparture: true })}
-								>
-									<Fontisto name='date' size={16} color='black' style={{ marginRight: 10 }} />
-									<Text>{departure.format('DD-MM-YYYY')}</Text>
-									<Text style={styles.labelDate}>Дата отъезда</Text>
-								</TouchableOpacity>
-								{showDatePickerDeparture && (
-									<DateTimePicker
-										testID='dateTimePicker'
-										value={moment(departure).toDate()}
-										minimumDate={moment(arrival).toDate()}
-										is24Hour={true}
-										display='default'
-										onChange={(event, selectedDate) => {
-											this.setState({
-												departure: moment(selectedDate),
-												showDatePickerDeparture: false,
-											})
-										}}
-									/>
-								)}
-							</View>
-						</View>
-
-						<Text style={{ fontStyle: 'italic', marginLeft: 4, color: '#999', marginBottom: 10 }}>
-							{this.getDiffDays()}
-							<Text>
-								{this.state.type.id === this.props.route.params.variations.night.id
-									? ' ночь'
-									: ' сутки'}
-							</Text>
-						</Text>
-						<View style={styles.halfInput}>
-							<View style={{ flex: 1, marginRight: 10 }}>
-								<View style={styles.picker}>
-									<Picker
-										selectedValue={adults}
-										onValueChange={(itemValue, itemIndex) => this.setState({ adults: itemValue })}
-									>
-										<Picker.Item label='1 взрослых' value='1' />
-										<Picker.Item label='2 взрослых' value='2' />
-										<Picker.Item label='3 взрослых' value='3' />
-										<Picker.Item label='4 взрослых' value='4' />
-										<Picker.Item label='5 взрослых' value='5' />
-										<Picker.Item label='6 взрослых' value='6' />
-									</Picker>
-								</View>
-							</View>
-							<View style={{ width: '50%' }}>
-								<View style={styles.picker}>
-									<Picker
-										selectedValue={children}
-										onValueChange={(itemValue, itemIndex) => this.setState({ children: itemValue })}
-									>
-										<Picker.Item label='Без детей' value='0' />
-										<Picker.Item label='1 ребенок' value='1' />
-										<Picker.Item label='2 детей' value='2' />
-										<Picker.Item label='3 детей' value='3' />
-										<Picker.Item label='4 детей' value='4' />
-										<Picker.Item label='5 детей' value='5' />
-										<Picker.Item label='6 детей' value='6' />
-									</Picker>
-								</View>
-							</View>
-						</View>
-=======
->>>>>>> c91e3f3c593f6f9c5fda6fd0e282a277e7d9ddd9
 
 						<Text style={{ ...styles.title, marginTop: 30 }}>Личные данные:</Text>
-						<Input label="Имя" value={firstName} onChangeText={(value) => this.setState({ firstName: value })} />
-						<Input label="Телефон" type="input-mask" keyboardType="phone-pad" value={phone} onChangeText={(value) => this.setState({ phone: value })} />
+						<Input
+							label='Имя'
+							value={firstName}
+							onChangeText={(value) => this.setState({ firstName: value })}
+						/>
+						<Input
+							label='Телефон'
+							type='input-mask'
+							keyboardType='phone-pad'
+							value={phone}
+							onChangeText={(value) => this.setState({ phone: value })}
+						/>
 
 						<Text style={{ marginTop: 20, marginBottom: 15 }}>
-							<Text style={{ color: 'green' }}>ПРЕДОПЛАТА НЕ ТРЕБУЕТСЯ</Text> — платите на месте. Бесплатная отмена бронирования
+							<Text style={{ color: 'green' }}>ПРЕДОПЛАТА НЕ ТРЕБУЕТСЯ</Text> — платите на месте.
+							Бесплатная отмена бронирования
 						</Text>
 						<Text style={{ ...styles.price, textAlign: 'right' }}>
 							Цена: <Text>{this.getPrice(variations, type)}</Text> ₸
@@ -234,7 +158,13 @@ export default class BookingScreen extends Component {
 								if (!pushing) this.pushOrder()
 							}}
 						>
-							{pushing ? <ActivityIndicator size="small" color="#fff" /> : <Text style={{ color: '#fff', textAlign: 'center', fontSize: 16 }}>Забронировать</Text>}
+							{pushing ? (
+								<ActivityIndicator size='small' color='#fff' />
+							) : (
+								<Text style={{ color: '#fff', textAlign: 'center', fontSize: 16 }}>
+									Забронировать
+								</Text>
+							)}
 						</TouchableOpacity>
 					</KeyboardAvoidingView>
 				</ScrollView>
