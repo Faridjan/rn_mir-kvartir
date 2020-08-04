668cb717223c3af9bb20a8bc1f194f0433ef7b83
Heretic <fred.andersen7@gmail.com>
2020-08-05 00:08:36 +0600

    ios friendly map page (GET to POST method / Acitive indicator centered)

--- src/screens/food/MapLocationFoodScreen.js
+++ src/screens/food/MapLocationFoodScreen.js
@@ -1,60 +1,3 @@
-// import React, { Component } from 'react'
-// import { View, StyleSheet, Dimensions } from 'react-native'
-// import MapView, { MAP_TYPES, PROVIDER_DEFAULT, Marker } from 'react-native-maps'
-
-// const { width, height } = Dimensions.get('window')
-
-// const LATITUDE_PETROPAVLOVSK = 54.904081
-// const LONGITUDE_PETROPAVLOVSK = 69.1500506
-
-// const ASPECT_RATIO = width / height
-// const LATITUDE_DELTA = 0.005
-// const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
-
-// class MapLocationScreen extends React.Component {
-// 	constructor(props) {
-// 		super(props)
-// 		const { params } = props.route
-// 		this.state = {
-// 			region: {
-// 				latitude: params.address.lat ? params.address.lat : LATITUDE_PETROPAVLOVSK,
-// 				longitude: params.address.lng ? params.address.lng : LONGITUDE_PETROPAVLOVSK,
-// 				latitudeDelta: params.address.lat && params.address.lng ? LATITUDE_DELTA : 0.1,
-// 				longitudeDelta: LONGITUDE_DELTA,
-// 			},
-// 			address: params.address,
-// 		}
-// 	}
-// 	get mapType() {
-// 		return this.props.provider === PROVIDER_DEFAULT ? MAP_TYPES.STANDARD : MAP_TYPES.NONE
-// 	}
-// 	render() {
-// 		const title = this.state.address.address ? this.state.address.address : 'Петропавловск'
-// 		return (
-// 			<View>
-// 				<MapView
-// 					provider={this.props.provider}
-// 					style={styles.map}
-// 					scrollEnabled={true}
-// 					zoomEnabled={true}
-// 					pitchEnabled={true}
-// 					rotateEnabled={true}
-// 					initialRegion={this.state.region}
-// 				>
-// 					<Marker coordinate={this.state.region} title={title} />
-// 				</MapView>
-// 			</View>
-// 		)
-// 	}
-// }
-// export default MapLocationScreen
-
-// const styles = StyleSheet.create({
-// 	map: {
-// 		height: '100%',
-// 	},
-// })
-
 import * as React from 'react'
 import { View, ActivityIndicator, Dimensions } from 'react-native'
 import { WebView } from 'react-native-webview'
@@ -80,24 +23,35 @@ export default class MapLocationScreen extends React.Component {
 				<WebView
 					style={{ flex: 1 }}
 					source={{
-						uri: `
-							https://mk.inplus.kz/map-page.php?
-							lat=${lat}&
-							lng=${lng}&
-							name=${name}&
-							address=${address.address}&
-							phone1=${phone1}&
-							phone2=${phone2}
+						uri: 'https://mk.inplus.kz/map-page.php',
+						method: 'POST',
+						body: `
+							{
+								"lat": "${lat}",
+								"lng": "${lng}",
+								"name": "${name}",
+								"address": "${address.address}",
+								"phone1": "${phone1}",
+								"phone2": "${phone2}"
+							}
 						`,
 					}}
 					onLoad={() => this.setState({ visible: false })}
 				/>
 				{this.state.visible && (
-					<ActivityIndicator
-						style={{ position: 'absolute', top: height / 2, left: width / 2 }}
-						size='large'
-						color='red'
-					/>
+					<View
+						style={{
+							position: 'absolute',
+							top: 0,
+							left: 0,
+							right: 0,
+							bottom: 0,
+							justifyContent: 'center',
+							alignItems: 'center',
+						}}
+					>
+						<ActivityIndicator size='large' color='red' />
+					</View>
 				)}
 			</View>
 		)
